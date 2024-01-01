89b3443299419328a38cb4d889bebb1c61d554cb
Reycko <78082869+Reycko@users.noreply.github.com>
2024-01-01 10:06:26 +0100

    Fixed battle/calming buffs getting spawn rate multiplied/divided by 4 instead of 2

--- CustomSpawnRate.cs
+++ CustomSpawnRate.cs
@@ -17,8 +17,6 @@ namespace CustomSpawnRate
 
 	public class ModConfigSpawnRates : ModConfig
 	{
-        //TODO: Toggle to set spawnrates to normal when a boss is spawned
-
 		public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [DefaultValue(1)]
@@ -76,8 +74,12 @@ namespace CustomSpawnRate
             // We're setting the max spawns here to prevent it being wrong due to setting it later overwriting the current value.
            maxSpawns = (config.CustomMaxSpawnsToggle ? config.CustomMaxSpawns : spawnRate * config.SpawnRate); // If the user has enabled custom max spawns, use that value instead
 
-            // This adds the Calming/Battle potion multiplier. Priority is Battle > Calming > Neither.
-            double calmingOrBattle = (player.HasBuff(13) ? 2 : player.HasBuff(106) ? 0.5 : 1); // Battle Buff ID is 13, Calming Buff ID is 106.
+            /* 
+             * This would add the Calming/Battle potion multiplier. Priority was Battle > Calming > Neither.
+             * However, Terraria does this with the EditSpawnRate() call, rendering this useless (it multiplies enemy spawns by 4x | .25x instead of 2x | 0.5x).
+             double calmingOrBattle = (player.HasBuff(13) ? 2 : player.HasBuff(106) ? 0.5 : 1); // Battle Buff ID is 13, Calming Buff ID is 106.
+            */
+
 
             /* 
              * Set spawn rate.
@@ -86,7 +88,6 @@ namespace CustomSpawnRate
              * which returns 120 w/o Calming/Battle buffs, 60 w/ Battle buff, and 240 w/ Calming buff.
             */
             spawnRate /= System.Math.Max((int)System.Math.Floor(config.SpawnRate * calmingOrBattle), 1); // Since the return value is an integer, make sure to have a whole number.
-
         }
     }
 }
